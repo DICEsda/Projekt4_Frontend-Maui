1edf14cea2561cfa69e343e10b6a7e11bce7016d
ahmadchiha01@gmail.com <ahmadchiha01@gmail.com>
2025-03-25 15:40:17 +0100

    Init

---
+++ FinanceTrackerAPP/FinanceTrackerAPP/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace FinanceTrackerAPP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ FinanceTrackerAPP/FinanceTrackerAPP/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FinanceTrackerAPP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
