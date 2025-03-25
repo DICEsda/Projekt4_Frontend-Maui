1d391f2a0628d0d049e4c076d4974bdb41295767
DICEsda <yahya24680@gmail.com>
2025-03-25 14:50:40 +0100

    Nyt Projekt tilføjet

---
+++ FinanceTracker-Projekt4/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FinanceTracker_Projekt4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FinanceTracker-Projekt4/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FinanceTracker_Projekt4
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
