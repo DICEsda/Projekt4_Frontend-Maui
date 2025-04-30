38ecaf3c3035f2584e0d699dbb87ebd1fbb76d9b
Ali Najafi <202307397@post.au.dk>
2025-04-30 15:11:03 +0200

    working on loghour

---
+++ FinanceTrackerAPP/Services/WorkshiftService.cs
@@ -0,0 +1,40 @@
+﻿using FinanceTracker.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Services
+{
+    public class WorkshiftService : IWorkshiftService
+    {
+        private readonly HttpClient _httpClient;
+
+        public WorkshiftService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("api");
+        }
+        public async Task<WorkshiftDTO> AddWorkShift(WorkshiftDTO workshift)
+        {
+            var response = await _httpClient.PostAsJsonAsync("Paycheck/registerWorkshift", workshift);
+
+            if (response.IsSuccessStatusCode) return await response.Content.ReadFromJsonAsync<WorkshiftDTO>();
+
+            return null;
+        }
+        public async Task<List<WorkshiftDTO>> GetAllWorkShifts()
+        {
+            var response = await _httpClient.GetAsync("Paycheck/GetAllWorkShiftsForUser");
+
+            if (response.IsSuccessStatusCode) return await response.Content.ReadFromJsonAsync<List<WorkshiftDTO>>();
+
+            return null;
+
+        }
+
+
+    }
+}
---
+++ FinanceTrackerAPP/ViewModels/LogHoursViewModel.cs
@@ -0,0 +1,46 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using FinanceTracker.DTO;
+using FinanceTracker.Services;
+using Plugin.Maui.Calendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.ViewModels
+{
+    public partial class LogHoursViewModel : ObservableObject
+    {
+        private readonly IWorkshiftService _workshiftService;
+        public ObservableCollection<WorkshiftDTO> Workshifts { get; set; }
+
+        public EventCollection Events { get; } = new EventCollection();
+
+
+        public LogHoursViewModel(IWorkshiftService workshiftService)
+        {
+            _workshiftService = workshiftService;
+        }
+
+        public async Task InitializeCalendar()
+        {
+
+            var workshifts = await _workshiftService.GetAllWorkShifts();
+            foreach (var workshift in workshifts)
+            {
+                var date = workshift.StartTime.Date;
+
+                Events[date] = new List<string> { "workshift" };
+
+            }
+        }
+
+
+
+
+
+
+    }
+}
