56e36d37a44a0edc1ecb783a4bcb59b97542f047
zebraflake <zebraflake@gmail.com>
2025-05-21 16:04:39 +0200

    take over khaled

---
+++ FinanceTrackerAPP/Localization.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Resources;
+
+namespace MyApp.Helpers;
+
+public static class Localization
+{
+    private static readonly ResourceManager ResourceManager = new ResourceManager("FinanceTracker.Resources.Strings.Resources", typeof(Localization).Assembly);
+
+    public static string Get(string key)
+    {
+        return ResourceManager.GetString(key, CultureInfo.CurrentUICulture) ?? $"[{key}]";
+    }
+}
